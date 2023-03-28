9c857af82221c78b9f657bc463fd978277df27d1
adamjmcgrath <adam.mcgrath@okta.com>
2023-03-28 17:59:16 +0100

    check default

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    AUTH0_BASE_URL: `${process.env.VERCEL_GIT_REPO_SLUG}-git-${process.env.VERCEL_GIT_COMMIT_REF}-${process.env.VERCEL_GIT_REPO_OWNER}.vercel.app`,
+    AUTH0_BASE_URL:
+      process.env.AUTH0_BASE_URL ||
+      `${process.env.VERCEL_GIT_REPO_SLUG}-git-${process.env.VERCEL_GIT_COMMIT_REF}-${process.env.VERCEL_GIT_REPO_OWNER}.vercel.app`,
   },
 };
 
