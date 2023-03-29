5e82f3f885d2a75e4cd8294d40b39e427fe6b314
adamjmcgrath <adam.mcgrath@okta.com>
2023-03-29 09:02:14 +0100

    tst

--- middleware.js
+++ middleware.js
@@ -1,22 +1,9 @@
 import { NextResponse } from "next/server";
-import { getSession } from "@auth0/nextjs-auth0/edge";
-
 export async function middleware(req) {
-  const { pathname } = req.nextUrl;
-
-  if (pathname === "/") {
-    const res = NextResponse.next();
-    const session = await getSession(req, res);
-    const loggedIn = Boolean(session);
-    if (!loggedIn) {
-      return NextResponse.redirect(
-        new URL(
-          `/guest?baseurl=${process.env.AUTH0_BASE_URL}&vercelurl=${process.env.VERCEL_URL}`,
-          req.url
-        )
-      );
-    }
-  }
+  return new NextResponse(`AUTH0_BASE_URL: ${process.env.AUTH0_BASE_URL}`, {
+    status: 200,
+    headers: { "content-type": "text/plain" },
+  });
 }
 
 export const config = {
